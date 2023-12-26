5a4d2ddc5a3e1d0de30dd9a117c8a2d437dd89c9
Dabbler <csturgell94@gmail.com>
2023-12-26 10:51:15 -0500

    Updated

--- Lesson 1 - Functions and Variables/Introductory.js
+++ Lesson 1 - Functions and Variables/Introductory.js
@@ -1,5 +1,46 @@
 
 We will start off with what functions are and how they are used.
 
+Below are a list of commonly used pre-defined functions.
+
+Pre-defined functions are functions that are provided by the Graal Engine to be used, they are common events of interaction.
+
+
+function onCreated() {
+  
+}
+
+function onPlayerChats() {
+
+}
+
+function onPlayerLogin() { 
+
+}
+
+
+
+Here is an example of a custom function
+
+function oncustomFunction() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
